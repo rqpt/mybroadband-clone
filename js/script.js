9ecdb58b84df9a27bf33f7b9ce8d275cf38b1967
rqpt <1ernstvermeulen1@gmail.com>
2023-01-16 22:09:30 +0200

    implemented drop down fixed header section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+myID = document.getElementById('sticky-title');
+
+const myScrollFunc = function() {
+    let y = window.scrollY;
+    if (y >= 100) {
+        myID.className = 'visible';
+    } else {
+        myID.className = 'hidden';
+    }
+};
+
+window.addEventListener('scroll', myScrollFunc);
